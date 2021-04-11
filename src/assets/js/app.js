ce54429d098b6b0dfa7eb2593a1fe134cb9c3d83
Victor Daniel Franco Valencia <vfrancov25@gmail.com>
2021-04-11 14:29:06 -0500

    Spread and Rest

---
+++ src/assets/js/app.js
@@ -0,0 +1,20 @@
+
+
+let librerias = ["Lib1", "Lib2", "Lib3"];
+let dependencies = ["dev1", "dev2", "dev3"];
+
+function importsLibs(...libs) {//rest
+    console.log(libs);
+}
+
+
+//librerias.forEach(lib => importsLibs(lib));
+importsLibs(librerias, dependencies);
+
+function spreadOperator(array) {
+    console.log(...array);
+    [...array].forEach(number => console.log(number));
+}
+
+let miArray = [1, 2, 4, 5];
+spreadOperator(miArray);
